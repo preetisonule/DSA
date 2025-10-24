cef87a22df558c7fd3c4f4d2e2e336f049f6c6a0
prretisonule <preetisonule3186@gmail.com>
2025-10-24 22:24:14 +0530

    Singly Linked list basic operation done.

---
+++ LinkedList/Deletion.cpp
@@ -0,0 +1,83 @@
+// all good
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    public:
+    Node(int data){
+        this->data = data;
+        next = nullptr;
+    }
+};
+
+Node* convertToLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node * mover = head;
+    for(int i = 1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+Node* delHead(Node* head){
+    if(head==NULL) return head;
+
+    Node* temp = head;
+    head = head->next;
+    free(temp);
+    return head;
+}
+
+Node* delTail(Node * head){
+    Node* temp = head;
+    while(temp->next->next!=nullptr){
+        temp = temp->next;
+    }
+    free(temp->next);
+    temp->next = nullptr;
+    return head;
+}
+
+Node* delPos(Node* head,int k){
+    Node* temp = head;
+    int i = 1;
+    while(i<k){
+        temp = temp->next;
+        i++;
+    }
+    Node* r = temp->next;
+    temp->next = temp->next->next;
+    free(r);
+    return head;
+
+}
+
+int main() 
+{
+    vector<int> arr = {12,1,3,14};
+    Node* head = convertToLL(arr);
+    Node* temp = head;
+
+    // Traverse original
+    while(temp!=nullptr){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+    // temp = delHead(head);
+    // temp = delTail(head);
+    temp = delPos(head,2);
+
+    // Traverse deleted head list
+    while(temp!=nullptr){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+
+     return 0;
+}
---
+++ LinkedList/Insertion.cpp
@@ -0,0 +1,72 @@
+    
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* convertToLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 0; i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+Node* insertHead(Node* head,int data){
+    Node* node = new Node(data);
+    Node* temp = head;
+    node->next=head->next;
+    head = node;
+    free(temp);
+    return head;
+}
+
+Node* insertTail(Node* head, int data){
+    Node* node = new Node(data);
+    Node* temp = head;
+     
+    return head;
+}
+
+Node* insertPos(Node* head, int data, int k){
+    Node* node = new Node(data);
+    Node* temp = head;
+    int i = 1;
+    while(i<k){
+        temp = temp->next;
+        i++;
+    }
+    node->next = temp->next;
+    temp->next = node;
+    return head;
+}
+
+int main() 
+{
+    vector<int> arr = {1,2,3, 4 ,7 ,9};
+    Node* head = convertToLL(arr);
+    // Traversing
+    // Node* temp = insertHead(head,7);
+    // Node* temp = insertTail(head,6);
+    Node* temp = insertPos(head,6,3);
+    temp = temp->next;
+    while(temp){
+        cout<<temp->data<<' ';
+        temp = temp->next;
+    }
+
+     return 0;
+}
